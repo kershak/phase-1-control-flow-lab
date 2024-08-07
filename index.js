33c89bcbc4713f454a73b1abb927362f70259604
NillsLopez <yakusaz17@gmail.com>
2024-08-07 11:56:17 -0400

    completed

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 function scuberGreetingForFeet(distance){
-  // Write your code here!
-  if (distance > 2500){
-    return ('No can do.')
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } 
+  else if (distance <=2000) {
+    return 'That will be twenty bucks.';
   }
-  else if (distance < 400){
-    return ('This one is on me!')
+  else if (distance <2500){
+    return 'I will gladly take your thirty bucks.';
   }
-  else if (distance <=2000 && distance >= 400){
-    return ('That will be twenty bucks.')
-  }
-  else if (distance > 2000 && distance < 2500){
-    return ('I will gladly take your thirty bucks.')
+  else {
+    return 'No can do.';
   }
 }
 
-function ternaryCheckCity(location){
-  // Write your code here!
-  let NYC;
-  return location === 'NYC' ? "Ok, sounds good.":"No go."
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : "No go.";
 }
 
-function switchOnCharmFromTip(value){
-  // Write your code here!
-  switch (value){
-  case 'generous':
-    return 'Thank you so much.'
-    break
-  case 'not as generous':
-    return 'Thank you.'
-    break
-  default: 
-    return 'Bye.'
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';  
   }
 }
